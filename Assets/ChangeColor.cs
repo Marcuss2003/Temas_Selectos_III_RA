6aedbdaa5c06c637464c6421656a23b847f66d4d
Marco <mgalvanromero82@gmail.com>
2026-03-15 12:03:30 -0600

    Entrega Practica 3

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    public GameObject model;
-    public Color color;
-    public Material ColorMaterial;
+    [Header("Configuración de Accesorios")]
+    public GameObject[] accesorios; 
+    private int indexAccesorioActual = 0;
 
-    void Start()
+    [Header("Partes del Cuerpo de Mordecai")]
+    public GameObject[] partesCuerpo; 
+    public Color[] colores; 
+
+    public void SiguienteAccesorio()
     {
-         
+        if (accesorios.Length < 2) return; 
+
+        accesorios[indexAccesorioActual].SetActive(false);
+        int nuevoIndex;
+        do {
+            nuevoIndex = Random.Range(0, accesorios.Length);
+        } while (nuevoIndex == indexAccesorioActual); 
+
+        indexAccesorioActual = nuevoIndex;
+        accesorios[indexAccesorioActual].SetActive(true);
     }
-    public void ChangeColor_BTN()
+
+    public void SiguienteColor()
     {
-        model.GetComponent<Renderer>().material.color = color; 
-        ColorMaterial.color = color;
+        if (colores.Length == 0 || partesCuerpo.Length == 0) return;
+
+        int indexAleatorio = Random.Range(0, colores.Length);
+        Color colorAzar = colores[indexAleatorio];
+
+        foreach (GameObject parte in partesCuerpo)
+        {
+            Renderer rend = parte.GetComponent<Renderer>();
+            if (rend != null)
+            {
+                
+                rend.material.color = colorAzar;
+            }
+        }
     }
 }
-
